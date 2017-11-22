4e9486ba3521705b78b470ecbe46952e07ba7021
jltahara <jltahara@ucsc.edu>
2017-11-21 23:40:43 -0800

    Finished

---
+++ lab5/Lab5.asm
@@ -0,0 +1,92 @@
+# Justin Tahara 10/22/17
+# Due 11/26/17
+# Lab 5 - Vigenere Cipher
+# 02C Michael Powell
+
+.data 
+Key: 		.asciiz "The given key is: "
+Text:		.asciiz "The given text is: "
+Encrypt: 	.asciiz "The encrypted text is: "
+Decrypt: 	.asciiz "The decreypted text is: "
+NL: 		.asciiz "\n"
+
+.text
+main:
+	lw	$t0, ($a1)	# Loads the given key in $t0
+	lw 	$t1, 4($a1)	# Loads the given text in $t1
+	li 	$v0, 4		# Sets up the displaying message
+	la 	$a0, Key	# Reads the message and stores in a0
+	syscall			# Do the syscall
+	la 	$a0, ($t0)	# Puts the user's key next to the text
+	syscall			# Do the syscall
+	li	$v0, 4		# Sets up for new line
+	la	$a0, NL		# Creates new line
+	syscall			# Do the syscall
+	li 	$v0, 4		# Sets up the displaying message
+	la 	$a0, Text	# Reads the message and stores in a0
+	syscall			# Do the syscall
+	la 	$a0, ($t1)	# Puts the user's input text next to the text
+	syscall			# Do the Syscall
+	li	$v0, 4		# Sets up for new line
+	la	$a0, NL		# Creates new line
+	syscall			# Do the syscall
+	li 	$v0, 4		# Sets up the displaying message
+	la 	$a0, Encrypt	# Prints next message
+	syscall			# Do the syscall
+	move	$t2, $t0	# Make a copy of the key in a separate terminal
+	move  	$t3, $t1	# Make a copy of the text in a separate terminal
+
+Encode:
+	lb 	$t4, ($t2)	# Takes the byte from the input key
+	lb	$t5, ($t3)	# Takes the byte from the input text
+	beqz	$t4, Looper	# Jumps to the looper if the byte in the key equals zero
+	beqz	$t5, Message	# Jumps to the next part if the text string ends
+	add	$t6, $t5, $t4 	# Adds the byte from the key and text
+	div 	$t7, $t6, 128	# Divides the sum by 128 which is the total amount of ascii values
+	mfhi 	$t7		# Stores the remainder in $t7
+	li  	$v0, 11		# Sets up to print out the ascii character
+	move 	$a0, $t7	# Move the value from $t7 to $a0
+	syscall			# Do the syscall
+	addi	$t2, $t2, 1	# Shift the byte of the key
+	addi	$t3, $t3, 1 	# Shift the byte of the text
+	j	Encode		# Jump back to the loop
+
+Looper:	
+	move	$t2, $t0	# Resets the key	
+	j	Encode		# Jump back to the loop
+		
+Message:
+	move	$t2, $t0	# Resets the key 
+	move	$t3, $t1	# Resets the text
+	li	$v0, 4		# Sets up for new line
+	la	$a0, NL		# Creates new line
+	syscall			# Do the syscall
+	li 	$v0, 4		# Sets up the displaying message
+	la 	$a0, Decrypt	# Prints the next message on the new line
+	syscall			# Do the syscall
+	
+Decode:		
+	lb 	$t4, ($t2)	# Takes the byte from the input key
+	lb	$t5, ($t3)	# Takes the byte from the input text
+	beqz	$t4, Looper2	# Jumps to the looper if the byte in the key equals zero
+	beqz	$t5, End	# Jumps to the end if the text byte ends
+	add	$t6, $t4, $t5	# Adds the byte of the key and text
+	div 	$t7, $t6, 128	# Divides the sum by 128 which is the total amount of ascii values
+	mfhi 	$t7		# Stores the remainder in $t7
+	sub	$t7, $t7, $t4	# Subtracts the key from the encrypted ascii value
+	add	$t7, $t7, 128	# Adds 128 back to the value 
+	li  	$v0, 11		# Sets up to print out the ascii value
+	move 	$a0, $t7	# Move the value from $t7 to $a0
+	syscall			# Do the syscall
+	addi	$t2, $t2, 1	# Shift the byte of the key
+	addi	$t3, $t3, 1 	# Shift the byte of the text
+	j	Decode		# Jumps back to the loop
+
+Looper2:	
+	move	$t2, $t0	# Resets the key 
+	j	Decode		# Jumps back to the loop
+				
+End:
+	li 	$v0, 10		# Ends the program
+	syscall			# Do the syscall
+	
--- lab5/README.txt
+++ lab5/README.txt
@@ -1,1 +1,13 @@
 Your Lab 5 work *must* be done in this directory.
+Justin Tahara
+jltahara@ucsc.edu 
+1524347
+Lab 5 - Vigenere Cipher
+02C Michael Powell
+Due 11/26/17
+
+For the encode algorithm I analyzed the given C code and made an algorithm that took the first byte of the key and the text and adds them together. Then once the sum is found it is divided by 128 which is the amount ascii characters there is. Once the sum is divided, the remainder is printed out which will be the encrypted text. The bytes for the key and text are shifted to the next byte over but the key has to repeat so there is another loop to reset the key bytes. Once the text gets to the last byte it moves on to the decoder.
+
+For the decoder the algorithm is very similar. The sums are found and it is divided by 128 like before. The remainder is the same number as the encoder but from there you subtract the value of the key which will result in a negative number. Once that number is stored, you add 128 again to the number stored to get the value of the decrypted value again.
+
+In order to make the algorithms to work I used the load word function in order to store the inputs into certain terminals and later on used move to store the inputed values into a separate terminal to make a copy. Using load byte, I got each individual byte and added them together. Stored the sum. Divided by 128 which was described before. As described before the functions for both the encode and decode changes but the numbers are usually temporarily saved and the only value that never changes is the inputed values from the user. 
